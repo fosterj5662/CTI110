c51bcff13a83b45131004269e78ad4b273fc0954
fosterj5662 <fosterj5662@student.faytechcc.edu>
2026-02-17 14:33:34 -0500

    Add files via upload

---
+++ P1LAB1_Foster.py
@@ -0,0 +1,12 @@
+# Jonathan foster
+# 2/12/26
+# P1LAB1 - CTI110
+# Creating python program using vscode
+
+# write the code to get user input
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+
+# display outputs
+print("Hello,",fname,lname + "! Welcome to CTI110")
