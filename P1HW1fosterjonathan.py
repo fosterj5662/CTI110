428c08275d736e78aa33e88c2010f48b6c779986
fosterj5662 <fosterj5662@student.faytechcc.edu>
2026-02-17 14:29:21 -0500

    Add files via upload

---
+++ P1HW1fosterjonathan.py
@@ -0,0 +1,26 @@
+# Jonathan Foster
+# 2/17/26
+# P1HW1
+# This program will perform basic math calculations
+
+print("-----Calculating Exponents----")
+print()
+print()
+base = int(input("Enter an integer as a base value: "))
+exponent = int(input("Enter an integer as a base value: "))
+print()
+print()
+#number = base ** exponent
+print(base, "raised to the power of", exponent, "is",base ** exponent, "!!")
+print()
+print()
+print("-----Addition and Subtraction----")
+print()
+print()
+start = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+sub = int(input("Enter an integer to subtract: "))
+print()
+print()
+print(start, "+" ,add, "-" ,sub, "is equal to", start + add - sub)
+
